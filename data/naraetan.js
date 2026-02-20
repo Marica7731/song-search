706e8746ebdbd94923682903e207ad88952e9bf4
Marica7731 <108105251+Marica7731@users.noreply.github.com>
2026-02-21 05:22:48 +0800

    Create naraetan.js

---
+++ data/naraetan.js
@@ -0,0 +1,11 @@
+// data/naraetan.js
+// 歌手：なれたん Naraetan Ch. 的数据
+window.SONG_DATA = window.SONG_DATA || [];
+
+window.SONG_DATA.push(
+    // ... 这里粘贴你之前转换器生成的那一大段数组对象 ...
+    // 示例：
+    { title: "愛をとりもどせ!!", artist: "クリスタルキング", collection: "外国人が歌う昭和の名曲", up: "なれたん Naraetan Ch.", link: "https://..." },
+    { title: "ペガサス幻想", artist: "MAKE-UP", collection: "外国人が歌う昭和の名曲", up: "なれたん Naraetan Ch.", link: "https://..." }
+    // ...
+);
